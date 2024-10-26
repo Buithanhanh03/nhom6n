34b847689cd0413a750cecf3e285dc5026e39095
chinhkpah1 <dnc04102003@gmail.com>
2024-10-26 19:16:35 +0700

    1 chut form BanHang

--- BanHang.Designer.cs
+++ BanHang.Designer.cs
@@ -28,20 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.masp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tensp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mau = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.madm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tonkho = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.anh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soluongdaban = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.masp,
+            this.tensp,
+            this.gia,
+            this.mau,
+            this.size,
+            this.madm,
+            this.tonkho,
+            this.anh,
+            this.soluongdaban});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 236);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Brown;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 39);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(26, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SẢN PHẨM";
+            // 
+            // masp
+            // 
+            this.masp.DataPropertyName = "masp";
+            this.masp.HeaderText = "Mã";
+            this.masp.Name = "masp";
+            this.masp.ReadOnly = true;
+            this.masp.Width = 50;
+            // 
+            // tensp
+            // 
+            this.tensp.DataPropertyName = "tensp";
+            this.tensp.HeaderText = "Tên";
+            this.tensp.Name = "tensp";
+            this.tensp.ReadOnly = true;
+            this.tensp.Width = 300;
+            // 
+            // gia
+            // 
+            this.gia.DataPropertyName = "gia";
+            this.gia.HeaderText = "Giá";
+            this.gia.Name = "gia";
+            this.gia.ReadOnly = true;
+            // 
+            // mau
+            // 
+            this.mau.DataPropertyName = "mau";
+            this.mau.HeaderText = "Màu";
+            this.mau.Name = "mau";
+            this.mau.ReadOnly = true;
+            this.mau.Width = 50;
+            // 
+            // size
+            // 
+            this.size.DataPropertyName = "size";
+            this.size.HeaderText = "Size";
+            this.size.Name = "size";
+            this.size.ReadOnly = true;
+            // 
+            // madm
+            // 
+            this.madm.DataPropertyName = "madm";
+            this.madm.HeaderText = "Mã danh mục";
+            this.madm.Name = "madm";
+            this.madm.ReadOnly = true;
+            // 
+            // tonkho
+            // 
+            this.tonkho.DataPropertyName = "tonkho";
+            this.tonkho.HeaderText = "Tồn kho";
+            this.tonkho.Name = "tonkho";
+            this.tonkho.ReadOnly = true;
+            // 
+            // anh
+            // 
+            this.anh.DataPropertyName = "anh";
+            this.anh.HeaderText = "Ảnh";
+            this.anh.Name = "anh";
+            this.anh.ReadOnly = true;
+            this.anh.Visible = false;
+            // 
+            // soluongdaban
+            // 
+            this.soluongdaban.DataPropertyName = "soluongdaban";
+            this.soluongdaban.HeaderText = "Đã bán";
+            this.soluongdaban.Name = "soluongdaban";
+            this.soluongdaban.ReadOnly = true;
+            this.soluongdaban.Visible = false;
             // 
             // BanHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 444);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "BanHang";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BanHang";
+            this.Load += new System.EventHandler(this.BanHang_Load);
+            this.Click += new System.EventHandler(this.BanHang_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn masp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tensp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mau;
+        private System.Windows.Forms.DataGridViewTextBoxColumn size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn madm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tonkho;
+        private System.Windows.Forms.DataGridViewTextBoxColumn anh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soluongdaban;
     }
 }
--- BanHang.cs
+++ BanHang.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,99 @@ namespace BTL_ThucTap_LTNET
 {
     public partial class BanHang : Form
     {
+        Panel panelDetail = new Panel();
+        Label lblInfo = new Label();
         public BanHang()
         {
             InitializeComponent();
+            panelDetail.Size = new Size(200, 30);
+            panelDetail.BorderStyle = BorderStyle.Fixed3D;
+            panelDetail.BackColor = Color.LightGray;
+            panelDetail.Visible = false;
+            lblInfo.AutoSize = true;
+            lblInfo.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Bold);
+            panelDetail.Controls.Add(lblInfo);
+            this.Controls.Add(panelDetail);
+            dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClick;
+            this.Click += BanHang_Click;
+        }
+        private SqlConnection conn = null;
+        string sqlqr = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Application.StartupPath}\qlbh_btl.mdf;Integrated Security=True;Connect Timeout=30";
+        private SqlConnection connectdb()
+        {
+            conn = new SqlConnection(sqlqr);
+            return conn;
+        }
+        private void LoadForm()
+        {
+            dataGridView1.Rows.Clear();
+            conn = connectdb();
+            string query = @"SELECT 
+                        sp.masp AS masp,
+                        sp.tensp AS tensp,
+                        ctdh.soluongdaban AS soluongdaban,
+                        sp.gia AS gia,
+                        sp.anh AS anh,
+                        sp.size AS size,
+                        sp.mau AS mau,
+                        sp.madm AS madm,
+                        sp.tonkho AS tonkho
+                    FROM 
+                        sanpham sp
+                    LEFT JOIN 
+                        chitietdonhang ctdh ON sp.masp = ctdh.masp";
+
+            using (conn)
+            using (SqlCommand command = new SqlCommand(query, conn))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+        private void BanHang_Load(object sender, EventArgs e)
+        {
+            LoadForm();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var row = dataGridView1.Rows[e.RowIndex];
+                string info = "";
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    if (cell.OwningColumn.HeaderText == "Tên" || cell.OwningColumn.HeaderText == "Đã bán")
+                    {
+                        info += cell.OwningColumn.HeaderText + ": " + cell.Value.ToString() + "\n";
+                    }
+                }
+
+                lblInfo.Text = info;
+
+                // Get the position of the DataGridView on the form
+                Point dataGridViewLocation = dataGridView1.Location;
+
+                // Calculate the y position for the panel
+                int rowHeight = dataGridView1.RowTemplate.Height; // Get the height of a row
+                int panelY = dataGridViewLocation.Y + row.Index * rowHeight;
+
+                // Set the panel's location
+                panelDetail.Location = new Point(dataGridViewLocation.X, panelY);
+                panelDetail.BringToFront();
+                panelDetail.Visible = true; // Make the panel visible
+            }
+        }
+
+        private void BanHang_Click(object sender, EventArgs e)
+        {
+            if (panelDetail.Visible)
+            {
+                panelDetail.Visible = false;
+                lblInfo.Text = ""; // Xóa thông tin trong label
+            }
         }
     }
 }
